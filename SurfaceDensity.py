9b52dc6208be644308df4ab1ff37e58c21ab96a4
TrystanScottLambert <TrystanScottLambert@gmail.com>
2021-08-04 12:37:16 +0200

    04082021

--- SurfaceDensity.py
+++ SurfaceDensity.py
@@ -47,8 +47,8 @@ def sersic1(R, Ie, Re):
 def gaussianFunction(x, a, x0, sigma, c):
     return a*np.exp(-(x-x0)**2/(2*sigma**2)) + c
 
-def twoGaussianFunctions(x, a1, x01, sigma1, a2, x02, sigma2, c):
-    return a1*np.exp(-(x-x01)**2/(2*sigma1**2)) + a2*np.exp(-(x-x02)**2/(2*sigma2**2)) +c
+def twoGaussianFunctions(x, a1, x01, sigma1, a2, x02, sigma2):
+    return a1*np.exp(-(x-x01)**2/(2*sigma1**2)) + a2*np.exp(-(x-x02)**2/(2*sigma2**2))
 
 
 def sum_region(center,radius_min,radius_max,data_array,error=False):
@@ -100,7 +100,7 @@ arcsec_area = arcsec_per_pixel**2  # area of a single pixel     # work out the a
 circles=[]
 binwidth_pix_arc = 0.259999999
 max_arc = 2.079999999
-binwidth_pix_arc = 0.1
+binwidth_pix_arc = 0.2
 max_arc = 1.8
 
 binwidth_pix = round(binwidth_pix_arc/arcsec_per_pixel)#10#10
@@ -177,27 +177,34 @@ def fitFunction(function,xData,yData,yUncertainty,nstd=3):
 	poptDown = popt - nstd * perr
 	return popt,pcov,perr,poptUp,poptDown
 
-popt,pcov,perr,popt_up,popt_dw = fitFunction(gaussianFunction,full_gauss_radii,full_gauss_vals,full_guass_vals_uncertainties,3)
 
 x = np.linspace(-2,2,1000)  
+
+popt,pcov,perr,popt_up,popt_dw = fitFunction(gaussianFunction,full_gauss_radii,full_gauss_vals,full_guass_vals_uncertainties,3)
 fit = gaussianFunction(x, *popt)
 fit_up = gaussianFunction(x, *popt_up)
 fit_dw = gaussianFunction(x, *popt_dw)
-plt.plot(x,fit)
+
+#popt,pcov,perr,popt_up,popt_dw = fitFunction(twoGaussianFunctions,full_gauss_radii,full_gauss_vals,full_guass_vals_uncertainties,3)
+#fit = twoGaussianFunctions(x, *popt)
+#fit_up = twoGaussianFunctions(x, *popt_up)
+#fit_dw = twoGaussianFunctions(x, *popt_dw)
+
+
+plt.plot(x,fit,lw=3,color='k')
 plt.fill_between(x,fit_up,fit_dw,alpha=2.5)
-plt.xlim(left=0)
 plt.show()
 
 
 x = np.linspace(0,2,1000)
 nstd = 1
-popt,pcov,perr,popt_up,popt_dw = fitFunction(sersic,new_radiis_arc,vals,vals_uncertainties,nstd)
+poptCII,pcovCII,perrCII,popt_upCII,popt_dwCII = fitFunction(sersic,new_radiis_arc,vals,vals_uncertainties,nstd)
 popt1,pcov1,perr1,popt_up1,popt_dw1 = fitFunction(sersic1,new_radiis_arc,vals,vals_uncertainties,nstd)
 
 plt.errorbar(new_radiis_arc,vals,yerr=vals_uncertainties,fmt='ro',label='Data')
-plt.plot(x,sersic(x,*popt),label=r'Sersic free "$m$" parameter')
-plt.plot(x,sersic1(x,*popt1),lw=2,color='k',label=r'Sersic $m=1$')
-plt.fill_between(x,sersic(x,*popt_up),sersic(x,*popt_dw),alpha=0.25,label=f'{nstd}' + r'$\sigma$')
+plt.plot(x,sersic(x,*poptCII),label=r'Free $m$, ' + r'$R_{e} = $'f'{round(poptCII[1],4)}')
+plt.plot(x,sersic1(x,*popt1),lw=2,color='k',label=r'$m=1$, $R_{e}$ = ' + f'{round(popt1[1],4)}')
+plt.fill_between(x,sersic(x,*popt_upCII),sersic(x,*popt_dwCII),alpha=0.25,label=f'{nstd}' + r'$\sigma$')
 plt.xlim(left=0)
 plt.xlabel('Radius ["]',fontsize=30)
 plt.ylabel(r'$\Sigma_{C_{II}}$/${\rm area}$',fontsize=30)
@@ -206,14 +213,14 @@ plt.minorticks_on()
 plt.tick_params(which='both', width=2) #this has to be a separate line because width can't be set when using which='minor'
 plt.tick_params(which='major', length=8, direction='in') #If you want tick marks on the opposite side also, add right=True
 plt.tick_params(which='minor', length=4, direction='in')
-plt.legend()
+plt.legend(fontsize=20)
 plt.show()
 
 
 
 ############ KILL ################
 
-for i in penis:
+'''for i in penis:
 	print(poes)
 
 fitter = modeling.fitting.LevMarLSQFitter()
@@ -276,7 +283,7 @@ print(f'2 Gauss: {gauss_2d_aic}')
 print(f'Sersic: {sersic_aic}')
 print('\n\n')
 print('Sersic Best Fit: ')
-print(popt)
+print(popt)'''
 
 
 
@@ -334,15 +341,15 @@ value=0
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)	
-ax1.plot(sers_x_c,sersic(sers_x_c,*popt_c)/np.max(sersic(sers_x_c,*popt_c)),lw=3,color='b',label=f'[CII], n = {round(popt_c[-1],3)} \n reff = {round(popt_c[1],3)}')
-ax1.errorbar(CII_radii,CII_vals/np.max(CII_vals),yerr=CII_undertainties/np.max(CII_vals),fmt='bo',lw=3)
+#ax1.plot(sers_x_c,sersic(sers_x_c,*popt_c)/np.max(sersic(sers_x_c,*popt_c)),lw=3,color='b',label=f'[CII], n = {round(popt_c[-1],3)} \n reff = {round(popt_c[1],3)}')
+#ax1.errorbar(CII_radii,CII_vals/np.max(CII_vals),yerr=CII_undertainties/np.max(CII_vals),fmt='bo',lw=3)
 
-f = open('CII_SurfaceProfile.txt','w')
-f.write('# CII Surface Profile data \n')
-f.write('# Radii, normalized flux, flux uncertainty \n')
-for j in range(len(CII_radii)):
-	f.write(f'{CII_radii[j]} {CII_vals[j]/np.max(CII_vals)} {CII_undertainties[j]/np.max(CII_vals)} \n')
-f.close()
+#f = open('CII_SurfaceProfile.txt','w')
+#f.write('# CII Surface Profile data \n')
+#f.write('# Radii, normalized flux, flux uncertainty \n')
+#for j in range(len(CII_radii)):
+#	f.write(f'{CII_radii[j]} {CII_vals[j]/np.max(CII_vals)} {CII_undertainties[j]/np.max(CII_vals)} \n')
+#f.close()
 
 ### Looping over all the filters ###
 for filt in filters:
@@ -378,19 +385,6 @@ for filt in filters:
 						# getting the world co-ordinate system  (for plotting mostly)
 	center_pix_world = wcs.wcs_pix2world([center_pix],0)    # getting the wcs of the center pixels
 
-	'''fig = plt.figure(figsize=(20,16))   			# plotting the circluar apetures on to the images
-	ax = fig.add_subplot(121,projection=wcs)
-	for radii in radiis:	
-		circle = plt.Circle(center_pix,radii,facecolor='None',color='r',fill=False)
-		ax.add_artist(circle)
-		ax.set_xlabel('Dec [dd:mm:ss]',fontsize=30)
-		ax.set_ylabel('RA [hh:mm:ss]',fontsize=30)
-	norm = simple_norm(data,'log')
-	if filt == 'gaia_corrected_rescaled_acs.fits':
-		ax.imshow(data,norm=norm,vmin=0,vmax=0.03)
-	else:
-		ax.imshow(data)'''
-
 
 	areas = []
 	sums = []
@@ -401,7 +395,10 @@ for filt in filters:
 		sums.append(current_sum)
 		areas.append(current_area)
 		uncertainties.append(current_uncertainty)
-		new_radiis.append((radiis[i+1]+radiis[i])/2)
+		if i == 0:
+			new_radiis.append(0)
+		else:
+			new_radiis.append((radiis[i+1]+radiis[i])/2)
 	areas = np.array(areas)
 	sums = np.array(sums)
 	uncertainties = np.array(uncertainties)
@@ -414,22 +411,9 @@ for filt in filters:
 		vals = vals - np.min(vals)
 		vals_uncertainties = vals_uncertainties - np.min(vals)
 
-	full_gauss_radii = np.append(-1*new_radiis_arc[::-1],new_radiis_arc)
-	full_gauss_vals = np.append(vals[::-1],vals)
-	full_guass_vals_uncertainties = np.append(vals_uncertainties[::-1],vals_uncertainties)
-
-	'''ax1 = fig.add_subplot(122)
-	ax1.errorbar(full_gauss_radii,full_gauss_vals,yerr=full_guass_vals_uncertainties,fmt='ro',lw=3)
-	ax1.set_xlabel('Radius ["]',fontsize=30)
-	ax1.set_ylabel(r'$\Sigma_{C_{II}}$/${\rm area}$',fontsize=30)
-	ax1.tick_params(axis='both',labelsize=20)
-	ax1.minorticks_on()
-	ax1.tick_params(which='both', width=2) #this has to be a separate line because width can't be set when using which='minor'
-	ax1.tick_params(which='major', length=8, direction='in') #If you want tick marks on the opposite side also, add right=True
-	ax1.tick_params(which='minor', length=4, direction='in')
-	#ax1.set_xlim(left=0)
-	ax1.grid()'''
-
+	full_gauss_radii = np.append(-1*new_radiis_arc[::-1][:-1],new_radiis_arc)
+	full_gauss_vals = np.append(vals[::-1][:-1],vals)
+	full_guass_vals_uncertainties = np.append(vals_uncertainties[::-1][:-1],vals_uncertainties)
 
 	fitter = modeling.fitting.LevMarLSQFitter()
 	model = modeling.models.Gaussian1D(amplitude=np.max(vals),mean=0.0,stddev=0.2)  #note using the mean and standed deviation of the redshifts. 
@@ -446,15 +430,6 @@ for filt in filters:
 
 	x = np.linspace(-2,2,1000)  #in arcseconds
 
-	'''ax1.plot(x,fitted_model(x),label='Gaussian Fit',lw=3,color='k')
-	ax1.plot(x,gg_fit(x),label='2 Gaussian Fit',lw=3,color='m')
-	ax1.plot(x,part_a_gauss(x),lw=2,ls='--',color='r')
-	ax1.plot(x,part_b_gauss(x),lw=2,ls='--',color='b')	
-
-	plt.show()
-
-	fig = plt.figure()
-	ax1 = fig.add_subplot(111)'''
 
 	sers_x = np.linspace(0,2,1000)
 	popt,pcov = curve_fit(sersic,plotting_radiis_arcsec,vals)
@@ -465,17 +440,6 @@ for filt in filters:
 	#ax1.plot(sers_x,sersic1(sers_x,*popt1),lw=2,color='k',label=f'n = 1')
 	sersic_aic = aic.aic(vals,sersic1(plotting_radiis_arcsec,*popt1),3)
 
-	'''ax1.set_xlim(left=0)
-	ax1.errorbar(full_gauss_radii,full_gauss_vals,yerr=full_guass_vals_uncertainties,fmt='ro',lw=3)
-	ax1.set_xlabel('Radius ["]',fontsize=30)
-	ax1.set_ylabel(r'$\Sigma_{UV}$/${\rm area}$',fontsize=30)
-	ax1.tick_params(axis='both',labelsize=20)
-	ax1.minorticks_on()
-	ax1.legend(fontsize=20)
-	ax1.tick_params(which='both', width=2) #this has to be a separate line because width can't be set when using which='minor'
-	ax1.tick_params(which='major', length=8, direction='in') #If you want tick marks on the opposite side also, add right=True
-	ax1.tick_params(which='minor', length=4, direction='in')
-	plt.show()'''
 
 ############ Reading in the UV image #####################
 
@@ -490,16 +454,18 @@ for filt in filters:
 
 
 	sers_x = np.linspace(0,2,1000)
-	popt,pcov = curve_fit(sersic,plotting_radiis_arcsec,vals)
+	popt,pcov = curve_fit(sersic,plotting_radiis_arcsec,vals,sigma=vals_uncertainties)
+	perr = np.sqrt(np.diag(pcov))
+	print(f'{filt}: {popt[0]} +- {perr[0]}')
 	
 	sersic_aic = aic.aic(vals,sersic(plotting_radiis_arcsec,*popt),3)
 
 	popt1,pcov1 = curve_fit(sersic1,plotting_radiis_arcsec,vals)
 	# Chose to plot n best fit of n = 1. 
 	#ax1.plot(sers_x,sersic1(sers_x,*popt1)/np.max(sersic1(sers_x,*popt1)),lw=3,color=colors[value],label=f'{labels[value]}, n = {1} \n reff = {round(popt1[1],3)}')
-	ax1.plot(sers_x,sersic(sers_x,*popt)/np.max(sersic(sers_x,*popt)),color=colors[value],lw=3,label=f'{labels[value]}, n = {round(popt[-1],3)} \n reff = {round(popt[1],3)}')
-	
-	ax1.errorbar(full_gauss_radii,full_gauss_vals/np.max(full_gauss_vals),color=colors[value],yerr=full_guass_vals_uncertainties/np.max(full_gauss_vals),fmt='o',lw=3)
+	norm_factor = np.max(sersic(sers_x,*popt))
+	ax1.plot(sers_x,sersic(sers_x,*popt)/norm_factor,color=colors[value],lw=3,label=f'{labels[value]}, n = {round(popt[-1],3)} \n reff = {round(popt[1],3)}')
+	ax1.errorbar(full_gauss_radii,full_gauss_vals/norm_factor,color=colors[value],yerr=full_guass_vals_uncertainties/norm_factor,fmt='o',lw=3)
 	sersic_aic = aic.aic(vals,sersic1(plotting_radiis_arcsec,*popt1),3)
 	if filt == 'resampled_gaia_corrected_hst_13641_07_wfc3_ir_f160w_sci (1).fits' :
 		f = open('F160W_SurfaceProfile.txt','w')
@@ -509,15 +475,21 @@ for filt in filters:
 			print('WIRITING')
 			f.write(f'{full_gauss_radii[j]} {full_gauss_vals[j]/np.max(full_gauss_vals)}  {full_guass_vals_uncertainties[j]/np.max(full_gauss_vals)} \n')
 		f.close()
-	#ax1.semilogy()
-	ax1.set_xlim(left=0)
-	ax1.set_xlabel('Radius ["]',fontsize=30)
-	ax1.set_ylabel(r'Normalized $\Sigma$ flux/${\rm area}$',fontsize=30)
-	ax1.tick_params(axis='both',labelsize=20)
-	ax1.minorticks_on()
-	ax1.legend(fontsize=20)
-	ax1.tick_params(which='both', width=2) #this has to be a separate line because width can't be set when using which='minor'
-	ax1.tick_params(which='major', length=8, direction='in') #If you want tick marks on the opposite side also, add right=True
-	ax1.tick_params(which='minor', length=4, direction='in')
+
 	value+=1
+
+norm_factor = np.max(sersic(sers_x,*poptCII))
+ax1.plot(sers_x,sersic(sers_x,*poptCII)/np.max(sersic(sers_x,*poptCII)),lw=4,color='k',label=r'Free $m$, ' + r'$R_{e} = $'f'{round(poptCII[1],4)}')
+plt.fill_between(x,sersic(x,*popt_upCII)/norm_factor,sersic(x,*popt_dwCII)/norm_factor,alpha=0.25,label=f'{nstd}' + r'$\sigma$')
+ax1.set_xlim(left=0)
+ax1.set_xlabel('Radius ["]',fontsize=30)
+ax1.set_ylabel(r'Normalized $\Sigma$ flux/${\rm area}$',fontsize=30)
+ax1.tick_params(axis='both',labelsize=20)
+ax1.minorticks_on()
+ax1.legend(fontsize=20)
+ax1.tick_params(which='both', width=2) #this has to be a separate line because width can't be set when using which='minor'
+ax1.tick_params(which='major', length=8, direction='in') #If you want tick marks on the opposite side also, add right=True
+ax1.tick_params(which='minor', length=4, direction='in')
 plt.show()
+
+
